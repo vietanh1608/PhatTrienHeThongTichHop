5513726abe65e2b64c12c833bb6c74b8d31ea04b
vietanh1608 <72064287+vietanh1608@users.noreply.github.com>
2020-10-17 20:08:43 +0700

    Delete Bai2.java

--- Bai2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Bai2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What's your name ?");
-        String str=scanner.nextLine();
-        System.out.println("Hi,I'm "+str);
-    }
-}
